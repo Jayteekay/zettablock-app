9d6552e155e4e9d1446959458e703953ef9f1029
Johnson Tinuoye <johnsontinuoye@gmail.com>
2022-10-06 02:19:24 +0100

    Add Sort toggle

--- src/components/table/subcomponents/Table.tsx
+++ src/components/table/subcomponents/Table.tsx
@@ -1,12 +1,25 @@
+import { useMemo, useState } from "react";
 import { API } from "../../../types/data/API";
 import Row from "./Row";
 
 type APITableProps = { data: API[] }
 const Table = ({ data }: APITableProps) => {
+    const [sortColumn, setSortColumn] = useState("");
+    const sortBy = (column: string) => setSortColumn(column);
+    const displayData = useMemo(() => sortColumn ? data.sort((a, b) => {
+        if (sortColumn === "name") {
+            return a[sortColumn].localeCompare(b[sortColumn])
+        }
+        if (sortColumn === "id") {
+            return +a[sortColumn] > +b[sortColumn] ? 1 : -1
+        }
+        return 0;
+    }) : data, [sortColumn, data])
+
     return <table>
         <thead>
-            <th>ID</th>
-            <th>Name</th>
+            <th><button onClick={() => sortBy("id")}>ID</button></th>
+            <th><button onClick={() => sortBy("name")}>Name</button></th>
             <th>Operation Name</th>
             <th>Type</th>
             <th>Description</th>
@@ -14,7 +27,7 @@ const Table = ({ data }: APITableProps) => {
             <th>Updated at</th>
         </thead>
         <tbody>
-            {data.map(rowData => <Row data={rowData} />)}
+            {displayData.map(rowData => <Row key={rowData.id} data={rowData} />)}
         </tbody>
     </table>
 }
